ce7b2c7592b44fe760ebdf8ee772bbc685c63efe
NITIN SINGH <nitin072005singh@gmail.com>
2026-03-03 11:20:20 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (48.74%) - LeetSync

---
+++ 3312-number-of-changing-keys/number-of-changing-keys.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def countKeyChanges(self, s):
+        s = s.lower()
+        count = 0
+        for i in range(1,len(s)):
+            if s[i]!=s[i-1]:
+                count+=1
+        
+        return count
+              
+        
+       
