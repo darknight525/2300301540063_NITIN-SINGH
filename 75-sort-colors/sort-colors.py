ec31483adc4fd470831e2fad5851a0b39ff8ba07
NITIN SINGH <nitin072005singh@gmail.com>
2026-03-12 09:40:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (99.93%) - LeetSync

---
+++ 75-sort-colors/sort-colors.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def sortColors(self, nums):
+
+        l = 0
+        i = 0
+        r = len(nums) - 1
+
+        while i <= r:
+
+            if nums[i] == 0:
+                nums[l], nums[i] = nums[i], nums[l]
+                l += 1
+                i += 1
+
+            elif nums[i] == 2:
+                nums[r], nums[i] = nums[i], nums[r]
+                r -= 1
+
+            else:
+                i += 1
+        
+           
