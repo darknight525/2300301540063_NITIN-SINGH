19ac4a1bcc905a4900b498d6c43c721aa4d44815
NITIN SINGH <nitin072005singh@gmail.com>
2026-06-26 15:06:44 +0530

    Time: 31 ms (68.39%) | Memory: 18.8 MB (22.66%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -7,23 +7,44 @@
 class Solution(object):
     def detectCycle(self, head):
         slow = fast = head
+
         
+
         # Step 1: detect cycle
+
         while fast and fast.next:
+
             slow = slow.next
+
             fast = fast.next.next
+
             
+
             if slow == fast:
+
                 break
+
         else:
+
             return None   
+
         
+
         # Step 2: find start
+
         slow = head
+
         
+
         while slow != fast:
+
             slow = slow.next
+
             fast = fast.next
+
         
+
         return slow
+
         
+       
