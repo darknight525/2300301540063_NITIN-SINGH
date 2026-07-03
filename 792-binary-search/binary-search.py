526a724054dd1f5360d6dd056800888c1984071a
NITIN SINGH <nitin072005singh@gmail.com>
2026-07-03 15:01:12 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (95.59%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,19 +1,19 @@
 class Solution(object):
     def search(self, nums, target):
-        
-        left = 0
-        right = len(nums) - 1
-        
-        while left <= right:
-            mid = (left + right) // 2
-            
+        low = 0
+        high = len(nums)-1
+
+        while low <= high:
+            mid = (low + high)//2
+
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
-                left = mid + 1
+                low = mid + 1
             else:
-                right = mid - 1
+                high = mid - 1
         
         return -1
-       
-           
+                
+
+        
