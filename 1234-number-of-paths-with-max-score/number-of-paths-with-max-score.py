f7609055db348e58dc94a92f7e086dcbbb07f69c
NITIN SINGH <nitin072005singh@gmail.com>
2026-07-05 20:45:30 +0530

    Time: 113 ms (100.00%) | Memory: 13 MB (66.67%) - LeetSync

---
+++ 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.py
@@ -0,0 +1,50 @@
+class Solution:
+    def pathsWithMaxScore(self, board):
+
+        MOD = 10**9 + 7
+        n = len(board)
+
+        score = [[-1] * n for _ in range(n)]
+        ways = [[0] * n for _ in range(n)]
+
+        score[n - 1][n - 1] = 0
+        ways[n - 1][n - 1] = 1
+
+        for i in range(n - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+
+                if board[i][j] == "X":
+                    continue
+
+                if i == n - 1 and j == n - 1:
+                    continue
+
+                best = -1
+                cnt = 0
+
+                for x, y in ((i + 1, j), (i, j + 1), (i + 1, j + 1)):
+
+                    if x < n and y < n and score[x][y] != -1:
+
+                        if score[x][y] > best:
+                            best = score[x][y]
+                            cnt = ways[x][y]
+
+                        elif score[x][y] == best:
+                            cnt = (cnt + ways[x][y]) % MOD
+
+                if best == -1:
+                    continue
+
+                add = 0
+                if board[i][j].isdigit():
+                    add = int(board[i][j])
+
+                score[i][j] = best + add
+                ways[i][j] = cnt
+
+        if ways[0][0] == 0:
+            return [0, 0]
+
+        return [score[0][0], ways[0][0]]
+        
