2f04da5c837535f76c2b62dcabed7619a04ee4ff
NITIN SINGH <nitin072005singh@gmail.com>
2026-07-10 12:33:02 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (12.91%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
@@ -6,30 +6,35 @@ class Solution:
         if not root:
             return []
 
-        q = deque([root])
-        res = []
+        queue = deque([root])
+
+        ans = []
+
         leftToRight = True
 
-        while q:
+        while queue:
 
             level = []
 
-            for _ in range(len(q)):
+            size = len(queue)
 
-                node = q.popleft()
+            for i in range(size):
+
+                node = queue.popleft()
+
                 level.append(node.val)
 
                 if node.left:
-                    q.append(node.left)
+                    queue.append(node.left)
 
                 if node.right:
-                    q.append(node.right)
+                    queue.append(node.right)
 
             if not leftToRight:
                 level.reverse()
 
-            res.append(level)
+            ans.append(level)
 
             leftToRight = not leftToRight
 
-        return res
+        return ans
