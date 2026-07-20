e1c79756f15e51b42ef75253e33d633884e2304b
NITIN SINGH <nitin072005singh@gmail.com>
2026-07-20 13:20:54 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (15.79%) - LeetSync

---
+++ 1386-shift-2d-grid/shift-2d-grid.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def shiftGrid(self, grid, k):
+        m = len(grid)
+        n = len(grid[0])
+
+        arr = []
+
+        for row in grid:
+            arr.extend(row)
+
+        total = m * n
+        k %= total
+
+        arr = arr[-k:] + arr[:-k]
+
+        ans = []
+        index = 0
+
+        for i in range(m):
+            ans.append(arr[index:index + n])
+            index += n
+
+        return ans
+        
