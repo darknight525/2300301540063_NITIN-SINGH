32f4e0daae20d14d76e5664a473ffb76e272fd90
NITIN SINGH <nitin072005singh@gmail.com>
2026-07-23 12:55:44 +0530

    Time: 3 ms (31.58%) | Memory: 19.5 MB (65.31%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        def dfs(path):
+            if len(path) == len(nums):
+                ans.append(path[:])
+                return
+            for num in nums:
+                if num not in path:
+                    path.append(num)
+                    dfs(path)
+                    path.pop()
+
+        dfs([])
+        return ans
+     
